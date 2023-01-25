53b624c923b77ee1cf3cbab607c7bccabf066d81
Sardelka9515 <sardelka9515@gmail.com>
2023-01-25 11:12:47 +0800

    Update source generator

---
+++ src/ScriptHookVDotNetCore.Generator/Debug.cs
@@ -0,0 +1,15 @@
+﻿global using static ScriptHookVDotNetCore.Generator.Debug;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ScriptHookVDotNetCore.Generator
+{
+    internal static class Debug
+    {
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type = default);
+
+    }
+}
--- src/ScriptHookVDotNetCore.Generator/SymbolPopulator.cs
+++ src/ScriptHookVDotNetCore.Generator/SymbolPopulator.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
@@ -9,38 +10,30 @@ namespace ScriptHookVDotNetCore.Generator
 {
     public class SymbolPopulator
     {
-        public static IEnumerable<ITypeSymbol> AllTypes;
-        public static IEnumerable<IMethodSymbol> AllMethods;
-        public GeneratorExecutionContext Ctx;
+        public List<INamedTypeSymbol> AllClasses = new();
+        public List<IMethodSymbol> AllMethods = new();
         public void Populate(GeneratorExecutionContext context)
         {
-            AllTypes = GetAllTypes(context.Compilation);
-            AllMethods = GetAllStaticMethods(context.Compilation);
+            var comp = context.Compilation;
+            foreach (var tree in comp.SyntaxTrees)
+            {
+                var vis = new Visitor();
+                var model = context.Compilation.GetSemanticModel(tree);
+                vis.Visit(tree.GetRoot());
+                AllClasses.AddRange(vis.classes.Select(x => model.GetDeclaredSymbol(x)));
+            }
+            AllClasses = AllClasses.Distinct(SymbolEqualityComparer.Default).Cast<INamedTypeSymbol>().ToList();
+            AllMethods = AllClasses.SelectMany(c => c.GetMembers().OfType<IMethodSymbol>()).ToList();
         }
 
-        IEnumerable<IMethodSymbol> GetAllStaticMethods(Compilation compilation)
-            => GetAllTypes(compilation).SelectMany(x => x.GetMembers().OfType<IMethodSymbol>()).Where(x => x.IsStatic);
-
-        IEnumerable<INamedTypeSymbol> GetAllTypes(Compilation compilation)
-            => GetAllTypes(compilation.GlobalNamespace);
-
-        IEnumerable<INamedTypeSymbol> GetAllTypes(INamespaceSymbol @namespace)
+        class Visitor : CSharpSyntaxRewriter
         {
-            foreach (var type in @namespace.GetTypeMembers())
-                foreach (var nestedType in GetNestedTypes(type))
-                    yield return nestedType;
-
-            foreach (var nestedNamespace in @namespace.GetNamespaceMembers())
-                foreach (var type in GetAllTypes(nestedNamespace))
-                    yield return type;
-        }
-
-        IEnumerable<INamedTypeSymbol> GetNestedTypes(INamedTypeSymbol type)
-        {
-            yield return type;
-            foreach (var nestedType in type.GetTypeMembers()
-                .SelectMany(nestedType => GetNestedTypes(nestedType)))
-                yield return nestedType;
+            public List<ClassDeclarationSyntax> classes = new();
+            public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
+            {
+                classes.Add(node);
+                return node;
+            }
         }
     }
 }
