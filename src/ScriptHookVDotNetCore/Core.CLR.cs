91ea2b40e2a390e01b6f3f84649cee435a50da74
Sardelka9515 <sardelka9515@gmail.com>
2023-03-06 17:01:35 +0800

    Move loader class to a seperate file

--- src/ScriptHookVDotNetCore/Core.CLR.cs
+++ src/ScriptHookVDotNetCore/Core.CLR.cs
@@ -12,8 +12,6 @@ using System.Runtime.Loader;
 
 namespace SHVDN
 {
-
-
     /// <summary>
     /// The CoreCLR plugin loader implementations
     /// </summary>
@@ -21,7 +19,7 @@ namespace SHVDN
     {
         #region Main assembly only
 
-        static readonly Dictionary<string, Loader> _loaders = new(StringComparer.OrdinalIgnoreCase);
+        static readonly Dictionary<string, ScriptDirectoryLoader> _loaders = new(StringComparer.OrdinalIgnoreCase);
 
         static readonly ConcurrentQueue<Action> _taskQueue = new();
         static BaseScript _baseScript;
@@ -115,7 +113,7 @@ namespace SHVDN
 
                 dir = Path.GetFullPath(dir);
                 Logger.Debug($"Loading scripts from {dir}");
-                var loader = new Loader(dir);
+                var loader = new ScriptDirectoryLoader(dir);
                 _loaders.Add(dir, loader);
                 loader.DoInit(AsiModule);
             }
@@ -260,114 +258,6 @@ namespace SHVDN
             }
         }
 
-        class Loader : PluginLoader
-        {
-            public delegate void KeyEventDelegate(DWORD key, bool down, bool ctrl, bool shift, bool alt);
-            public Assembly ApiAssembly { get; }
-            public Action<IntPtr> DoInit { get; }
-            public Action<IntPtr> DoUnload { get; }
-            public Action<IntPtr> DoTick { get; }
-            public KeyEventDelegate DoKeyEvent { get; }
-            public Type CoreType { get; }
-            public object InvokeCore(string methodName, params object[] args)
-            {
-                var flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
-                return CoreType.GetMethod(methodName, flags).Invoke(null, args);
-            }
-            public Loader(string folder) : base(GetConfig(folder))
-            {
-                var flags = BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
-
-                ApiAssembly = LoadDefaultAssembly();
-                CoreType = ApiAssembly.GetType(typeof(Core).FullName);
-
-                var initMethod = getMethod(nameof(Core.OnInit));
-                Debug.Assert(initMethod != null);
-
-                var unloadMethod = getMethod(nameof(Core.OnUnload));
-                Debug.Assert(unloadMethod != null);
-
-                var tickMethod = getMethod(nameof(Core.DoTick));
-                Debug.Assert(tickMethod != null);
-
-                var keyEventMethod = getMethod(nameof(Core.DoKeyEvent));
-                Debug.Assert(keyEventMethod != null);
-
-                // Caching with delegates
-                DoInit = (Action<IntPtr>)Delegate.CreateDelegate(typeof(Action<IntPtr>), initMethod);
-                DoUnload = (Action<IntPtr>)Delegate.CreateDelegate(typeof(Action<IntPtr>), unloadMethod);
-                DoTick = (Action<IntPtr>)Delegate.CreateDelegate(typeof(Action<IntPtr>), tickMethod);
-                DoKeyEvent = (KeyEventDelegate)Delegate.CreateDelegate(typeof(KeyEventDelegate), keyEventMethod);
-
-                List<Assembly> scriptAssemblies = new();
-                foreach (var asmPath in Directory.GetFiles(folder, "*.dll").Where(IsManagedAssembly))
-                {
-                    // Skip loading of api assembly
-                    if (Path.GetFileNameWithoutExtension(asmPath) == typeof(Core).Assembly.GetName().Name)
-                        continue;
-
-                    try
-                    {
-                        Logger.Debug("Loading assembly: " + asmPath);
-                        scriptAssemblies.Add(LoadAssemblyFromPath(asmPath));
-                    }
-                    catch (Exception ex)
-                    {
-                        Logger.Error(ex.ToString());
-                    }
-                }
-
-                setProp(nameof(CurrentDirectory), folder);
-                setProp(nameof(ScriptAssemblies), scriptAssemblies.ToArray());
-                setProp(nameof(MainAssembly), typeof(Core).Assembly);
-                void setProp(string name, object value)
-                {
-                    var prop = CoreType.GetProperty(name, flags);
-                    Debug.Assert(prop != null, $"Property {name} not found");
-                    prop.SetValue(null, value);
-                }
-                MethodInfo getMethod(string name)
-                    => CoreType.GetMethod(name, flags);
-            }
-
-            protected override void Dispose(bool disposing)
-            {
-                if (!_disposed)
-                {
-                    DoUnload(default);
-                    _disposed = true;
-                }
-
-                // Call base class implementation.
-                base.Dispose(disposing);
-            }
-            static PluginConfig GetConfig(string folderPath)
-            {
-                var files = Directory.GetFiles(folderPath, "*.dll");
-                if (!files.Any(IsManagedAssembly))
-                    throw new FileNotFoundException("No managed assemblies were found in this folder");
-
-                var mainAssemblyPath = Path.Combine(folderPath, typeof(Core).Assembly.GetName().Name + ".dll"); // ScriptHookVDotNetCore.dll
-                mainAssemblyPath = Path.GetFullPath(mainAssemblyPath);
-
-                bool alwaysCopy = false;
-#if DEBUG
-                alwaysCopy = true;
-#endif
-                // Copy the API assembly if there's none
-                if (!File.Exists(mainAssemblyPath) || alwaysCopy)
-                    File.Copy(typeof(Core).Assembly.Location, mainAssemblyPath, true);
-
-                // Don't use shared type to allow different version of API assembly to be loaded at the same time
-                return new(mainAssemblyPath)
-                {
-                    EnableHotReload = false,
-                    IsUnloadable = true,
-                    LoadInMemory = true,
-                    PreferSharedTypes = false
-                };
-            }
-        }
 
         internal static void PrintAllScripts()
         {
